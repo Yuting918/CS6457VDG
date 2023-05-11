66325d6621ee96f2a92e2c34003517686d7c3251
Jeff Wilson <jeff@imtc.gatech.edu>
2023-05-11 11:50:51 -0400

    docs updated, some code cleanup

--- Assets/Scripts/AppEvents/CrateCollisionReporter.cs
+++ Assets/Scripts/AppEvents/CrateCollisionReporter.cs
@@ -12,14 +12,10 @@ public class CrateCollisionReporter : MonoBehaviour
         {
             //we'll just use the first contact point for simplicity
             EventManager.TriggerEvent<BoxCollisionEvent, Vector3, float>(c.contacts[0].point, c.impulse.magnitude);
+
+            // Note that BombBounceEvent only takes one arg, not two. EventManager.TriggerEvent<BombBounceEvent, Vector3>(....
         }
             
 
-//				foreach (ContactPoint contact in c.contacts) {
-//
-//						if (c.impulse.magnitude > 0.5f)
-//								EventManager.TriggerEvent<AudioEventManager.BoxAudioEvent, Vector3> (contact.point);
-//						
-//				}
     }
 }
